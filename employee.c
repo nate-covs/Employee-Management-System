8a91547e7e3fa2816b00bc8b44e19d27ca4656d1
Nate Covs <natecovs04@gmail.com>
2025-01-08 16:19:21 -0700

    Worked on removing employees now, very functional, will clean up soon.

--- employee.c
+++ employee.c
@@ -44,7 +44,7 @@ void insertRec() {
         && (strncmp(e.department, "research", 8) != 0) 
         && (strncmp(e.department, "sales", 5) != 0)) {
 
-            printf("Error! Incorrect usage, please enter department as listed above.\n");
+            printf("Error! Incorrect usage, please enter department as listed above:\n");
 
             fgets(e.department, sizeof(e.department), stdin);
             trimNewline(e.department);
@@ -107,7 +107,97 @@ void insertRec() {
 
 */
 
-void deleteRec();         
+void deleteRec() {
+    // want to be able find employee in department, or insert name and list employees with that name in departments
+
+    printf("-------------\n");
+    printf("|Departments|\n");
+    printf("-------------\n");
+
+
+    printf("managment\n");
+    printf("manufacturing\n");
+    printf("research\n");
+    printf("sales\n");
+
+    printf("Enter department name listed above of employee to delete:");
+
+    fgets(e.department, sizeof(e.department), stdin);
+    trimNewline(e.department);
+
+    while ((strncmp(e.department, "management", 10) != 0) 
+        && (strncmp(e.department, "manufacturing", 13) != 0)
+        && (strncmp(e.department, "research", 8) != 0) 
+        && (strncmp(e.department, "sales", 5) != 0)) {
+
+            printf("Error! Incorrect usage, please enter department as listed above:\n");
+
+            fgets(e.department, sizeof(e.department), stdin);
+            trimNewline(e.department);
+    }
+
+    
+
+    char filename[20]; // Make sure it's large enough to hold the full filename
+    snprintf(filename, sizeof(filename), "%s.txt", e.department); // Department name as filename
+
+    FILE *delete; 
+    delete = fopen(filename, "r+");
+
+    if(delete == NULL){
+        printf("Error opening file.");
+        return -1; 
+    }
+
+    printf("Enter employee name to be removed: ");
+    char name[40];
+    fgets(name, sizeof(name), stdin);
+
+    trimNewline(name);
+
+    char line[256];
+    long pos = ftell(delete);
+
+    FILE *temp;
+    temp = fopen("temp.txt", "w");
+
+    if(temp == NULL){
+        printf("Error opening file.");
+        return -1; 
+    }
+
+    while (fgets(line, sizeof(line), delete) != NULL) {
+        if (strstr(line, name) != NULL) {
+            printf("Deleting line with name: %s\n", line);
+            
+            // Skip next 3 lines
+            for (int i = 0; i < 4; i++) {
+                if (fgets(line, sizeof(line), delete) == NULL) {
+                    break;  // If there are fewer than 4 lines remaining, break out of the loop
+                }
+            }
+        } 
+        else {
+            fputs(line, temp);
+        }
+    }
+    
+    fclose(temp);
+    fclose(delete);
+
+    if(remove(filename) == 0){
+        if(rename("temp.txt", filename) == 0){
+            printf("Employee and old log deleted successfully.\n");
+        }
+        else{
+            printf("Error renaming new log file.\n");
+        }
+    }
+    else{
+        printf("Error removing employee and old log file.\n");
+    }
+
+}         
 
 void findRec();           
 
